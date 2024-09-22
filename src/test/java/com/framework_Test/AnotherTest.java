31acb28b5656fd98271fedd168c16e5ae17b56a9
Rubal Ahmed <ahmedrubal92@gmail.com>
2024-09-22 11:26:14 -0400

    another test added

---
+++ src/test/java/com/framework_Test/AnotherTest.java
@@ -0,0 +1,10 @@
+package com.framework_Test;
+
+import org.junit.jupiter.api.Test;
+
+public class AnotherTest {
+    @Test
+    public void justPractice(){
+        System.out.println("another line of code added");
+    }
+}
